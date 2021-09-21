ca92528a3db7c00058a660b6cb70f879ea8d6c92
akmchnkv <87667725+akmchnkv@users.noreply.github.com>
2021-09-21 18:45:39 +0300

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+	int x, y;
+	scanf("%d%d", &x, &y);
+	printf("adition = %d \n", x+y);
+	printf("multiplication = %d \n", x*y);
+	printf("subtration = %d \n", x-y);
+	printf("division = %d \n", x/y);
+}
